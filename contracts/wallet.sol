e8716c8874cff82c413065b0ccba13345e219b94
Addis Belete <addis.bel@gmail.com>
2022-09-20 22:24:00 +0300

    Add wallet.ts file

--- contracts/wallet.sol
+++ contracts/wallet.sol
@@ -3,7 +3,10 @@ pragma solidity ^0.8.0;
 
 contract Wallet {
     event Sent(address from, address to, uint256);
-
+/*
+@function - used to transfer Ether
+@param to - is the destination of Ether
+*/
     function send(address to) public payable {
         require(msg.value > 0, "please input a value");
         require(msg.value >= (msg.sender).balance, "Insufficient Balance");
@@ -14,6 +17,8 @@ contract Wallet {
     function balance() public view returns (uint256) {
         return msg.sender.balance;
     }
-function createAccount() public returns(address){}
-function ImportAccount() public {}
+
+    function createAccount() public returns (address) {}
+
+    function ImportAccount() public {}
 }
